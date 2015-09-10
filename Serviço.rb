f94da5ae126b636899eb9bfdb2d112ded5f88b1a
constantinologia <constantinologia@gmail.com>
2015-09-10 18:31:33 -0300

    Update Serviço.rb

--- Serviço.rb
+++ Serviço.rb
@@ -2,6 +2,13 @@ require "sinatra"
 require "rexml/document"
 include REXML
 
+
+#get 	/api/copas 	pega todas as copas
+#get 	/api/copas/:id 	pega uma copa pelo id
+#post 	/api/copas 	cria uma nova copa
+#put 	/api/copas/:id 	atualiza uma copa específica pelo id
+#delete 	/api/copas/:id 	deleta uma copa pelo id
+
 get '/' do
   
   "Hello World!"
